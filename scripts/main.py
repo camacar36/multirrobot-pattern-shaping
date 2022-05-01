3b15f0c9fbc2e8bd2648372d140af93fdc3be7b3
Carlos Martínez Suárez <87413904+camacar36@users.noreply.github.com>
2022-05-01 12:15:49 +0200

    Add files via upload

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,6 @@
 from Robot import Robot
 from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Polygon
 
 import rospy
 import sys
@@ -18,55 +19,58 @@ def main():
     # Creation of first robot
     robot1 = Robot(robot_number)
 
-    # Final point to reach
-    point = [2,0]
+    while not rospy.is_shutdown():
 
-    # Settimg initial position and orientation of the robot
-    robot1.get_tf()
+        data = rospy.wait_for_message('/goal_points', Polygon)
 
+        point = [data.points[int(robot_number)-1].x, data.points[int(robot_number)-1].y]
 
-    # --- Previous orientation of the robot ---
-
-    # Orientate the robot to the desired point
-    robot1.face_to_point(point,0.1)
-
-
-    # --- Movement of the robot ---
-
-    actual_distance = robot1.get_distance_to_objective(point)
-    prev_distance = actual_distance
-
-    # While distance to the final point is more than 0.1
-    while actual_distance > 0.1:
-
-        # Move forward
+        # Settimg initial position and orientation of the robot
         robot1.get_tf()
+
+
+        # --- Previous orientation of the robot ---
+
+        # Orientate the robot to the desired point
+        robot1.face_to_point(point,0.1)
+
+
+        # --- Movement of the robot ---
+
         actual_distance = robot1.get_distance_to_objective(point)
-        robot1.move_forward(0.4)
-
-        detected, side = robot1.detect_obstacles()
-        if detected:
-            if side == 1: # Turn left
-                start_angle = robot1.get_actual_orientation()
-                while detected == True:
-                    detected, side = robot1.detect_obstacles()
-                    robot1.turn_left(0.2)
-            
-            elif side == -1: # Turn right
-                start_angle = robot1.get_actual_orientation()
-                while detected == True:
-                    detected, side = robot1.detect_obstacles()
-                    robot1.turn_right(0.2)
-
-        # If the previous distance is lower than actual distance, it means that the robot have passed near the point
-        # but not so close enough to stop (>0.1), so it reorientates robot and keeps moving
-        if prev_distance < actual_distance:
-            robot1.stop()
-            robot1.face_to_point(point,0.1)
-
         prev_distance = actual_distance
 
-    robot1.stop()
+        # While distance to the final point is more than 0.1
+        while actual_distance > 0.1:
+
+            # Move forward
+            robot1.get_tf()
+            actual_distance = robot1.get_distance_to_objective(point)
+            robot1.move_forward(0.4)
+
+            detected, side = robot1.detect_obstacles()
+            if detected:
+                if side == 1: # Turn left
+                    start_angle = robot1.get_actual_orientation()
+                    while detected == True:
+                        detected, side = robot1.detect_obstacles()
+                        robot1.turn_left(0.2)
+                
+                elif side == -1: # Turn right
+                    start_angle = robot1.get_actual_orientation()
+                    while detected == True:
+                        detected, side = robot1.detect_obstacles()
+                        robot1.turn_right(0.2)
+
+            # If the previous distance is lower than actual distance, it means that the robot have passed near the point
+            # but not so close enough to stop (>0.1), so it reorientates robot and keeps moving
+            if prev_distance < actual_distance:
+                robot1.stop()
+                robot1.face_to_point(point,0.1)
+
+            prev_distance = actual_distance
+
+        robot1.stop()
 
 
 if __name__ == "__main__":
